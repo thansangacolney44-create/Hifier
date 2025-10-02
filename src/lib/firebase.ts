c1fac85cd6d79469f117311cc3191a967e0bfd00
Thansanga <thansangacolney44@gmail.com>
2025-10-02 22:19:54 +0530

    Add files via upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "studio-9206476147-10f1d",
-  "appId": "1:187269746863:web:db4d787dd48478f6dfc45a",
-  "apiKey": "AIzaSyBv1yrT3Ct3I7Kgqu8uaL0Iu8ANlWN4B6o",
-  "authDomain": "studio-9206476147-10f1d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "187269746863"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
